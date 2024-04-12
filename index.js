08e2948d811d6a8195f50719348dcf275123ef87
leiDnedyA <aydendiel@gmail.com>
2024-04-12 12:22:24 -0400

    refactor: moved API routes to their own router + added auth router

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
+const passport = require('passport');
+const session = require('express-session');
 const cors = require('cors');
 const path = require('path');
 
-const { isValidURL } = require('./src/utils.js');
-const { swapCommonTLDs, deleteDomainChars } = require('./src/generate_similar_domains.js');
-const { dnsLookup } = require('./src/squatting_checks.js');
-const { compareIcons } = require('./src/icon_check.js');
-const { getSearchResultDomains } = require('./src/search_result_comparison.js');
+require('dotenv').config();
+
+const apiRouter = require('./routes/api');
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -14,6 +14,12 @@ const port = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.json());
 
+app.use(session({
+  secret: process.env.EXPRESS_SESSION_SECRET,
+  resave: false,
+  cookie: { secure: false }, /* Note: this should be changed when no longer prototyping */
+  saveUninitialized: true
+}));
 
 /* Middleware */
 
@@ -22,106 +28,13 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use(passport.authenticate('session'));
 
 /* Routes */
 
 app.use(express.static(path.join(__dirname, './frontend/dist/')));
+app.use(apiRouter);
 
-app.get('/api/domains', async (req, res) => {
-
-  const rawDomain = req.query.domain;
-
-  if (req.path.startsWith('/api/domains') && !rawDomain) {
-    return res.status(400).json({ error: 'Missing query param "domain"' });
-  }
-  if (!isValidURL(rawDomain)) {
-    return res.status(400).json({ error: 'Invalid domain' });
-  }
-
-  let domain;
-
-  if (rawDomain.startsWith('http://') || rawDomain.startsWith('https://')) {
-    domain = new URL(rawDomain).host;
-  } else{
-    domain = new URL('http://' + rawDomain).host;
-  }
-
-  console.log(`Request made for domain "${domain}"`);
-
-  // // Example:
-  // const domains = [
-  //   {
-  //     domain: req.query.domain,
-  //     ipAddress: '192.168.1.100',
-  //     urlConstruction: 'legitimate',
-  //     category: 'business',
-  //     logoDetected: true,
-  //     riskLevel: 1,
-  //   },
-  // ];
-
-  const result = []; // return value, only records with URLS that don't 404
-  const allRecords = []; // stores all records, even those that 404
-
-  const tldSwaps = swapCommonTLDs(domain);
-  const charDeletions = deleteDomainChars(domain);
-  
-  for (let tldSwap of tldSwaps) {
-    allRecords.push({
-      domain: tldSwap,
-      ipAddress: '',
-      urlConstruction: 'New TLD',
-      category: 'unknown',
-      logoDetected: false,
-      detectedInSearch: false,
-      riskLevel: 1,
-      redirectToOriginal: false
-    });
-  }
-
-  for (let charDeletion of charDeletions) {
-    allRecords.push({
-      domain: charDeletion,
-      ipAddress: '',
-      urlConstruction: 'Character deletion',
-      category: 'unknown',
-      logoDetected: false,
-      detectedInSearch: false,
-      riskLevel: 1,
-      redirectToOriginal: false
-    });
-  }
-
-  for (let record of allRecords) {
-    const ip = await dnsLookup(record.domain);
-    if (ip !== null) {
-      record.ipAddress = ip;
-      result.push(record);
-    }
-  }
-
-  // Check search engine for other associated with original domain
-  const fullDomainSearch = await getSearchResultDomains(domain);
-  const domainNameSearch = await getSearchResultDomains(domain.split('/')[0]);
-
-  console.log(domainNameSearch);
-
-
-  for (let record of result) {
-    record.logoDetected = await compareIcons(domain, record.domain);
-    record.detectedInSearch = fullDomainSearch.has(record.domain) || domainNameSearch.has(record.domain);
-    
-    if (record.logoDetected && record.detectedInSearch) {
-      record.riskLevel = 5;
-    } else if (record.logoDetected) {
-      record.riskLevel = 2;
-    } else if (record.detectedInSearch) {
-      record.riskLevel = 2;
-    }
-  }
-
-  res.json(result);
-});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
---
+++ routes/auth.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const crypto = require('crypto');
+const bodyParser = require('body-parser');
+
+const router = express.Router();
+router.use(bodyParser.urlencoded())
+
+const users = {
+ // username : { password: "password", secret: "whatever" }
+ "ayden": {id: "ayden", password: "pass", secret: "test"}
+}
+
+passport.use(new LocalStrategy(function verify(username, password, cb) {
+  if(!users.hasOwnProperty(username)) {
+    return cb(null, false, { message: 'Incorrect username or password' });
+  }
+  if(users[username].password === password)  {
+    return cb(null, users[username]);
+  }
+  return cb(null, false, { message: 'Incorrect username or password' });
+}));
+
+passport.serializeUser(function(user, cb) {
+  process.nextTick(function() {
+    console.log(user)
+    cb(null, { id: user.id, secret: user.secret });
+  });
+});
+
+passport.deserializeUser(function(user, cb) {
+  process.nextTick(function() {
+    console.log(user)
+    return cb(null, user);
+  });
+});
+
+
+router.get('/login', (req, res) => {
+  res.send(`
+      <h1>Sign in</h1>
+      <form action="/login/password" method="post">
+          <label for="username">Username</label>
+          <input id="username" name="username" type="text" autocomplete="username" required autofocus>
+          <label for="current-password">Password</label>
+          <input id="current-password" name="password" type="password" autocomplete="current-password" required>
+          <input type="submit">
+      </form>
+    `);
+});
+
+router.post('/login/password', passport.authenticate('local'), (req, res) => {
+  res.json(req.user);
+});
+
+module.exports = router;
